d02812fe895acd14c4154f23c102680d91d26447
Muchen He <mansurhe@outlook.com>
2017-12-24 12:26:39 -0800

    Tutorial B

--- Tutorials/Tutorial_B.py
+++ Tutorials/Tutorial_B.py
@@ -9,7 +9,11 @@ class Window(QtWidgets.QWidget):
     def init_ui(self):
         self.button1 = QtWidgets.QPushButton("-")
         self.button2 = QtWidgets.QPushButton("+")
+        self.button3 = QtWidgets.QPushButton("OK")
         self.label1 = QtWidgets.QLabel("0")
+
+        # Line edit
+        self.lineEdit = QtWidgets.QLineEdit()
 
         h_box = QtWidgets.QHBoxLayout()
         # h_box.addStretch()    # This adds the padding
@@ -21,6 +25,13 @@ class Window(QtWidgets.QWidget):
         v_box = QtWidgets.QVBoxLayout()
         v_box.addWidget(self.label1)
         v_box.addLayout(h_box)
+        
+        # Line edit layout
+        h_box_le = QtWidgets.QHBoxLayout()
+        h_box.addWidget(self.lineEdit)
+        h_box.addWidget(self.button3)
+        v_box.addChildLayout(h_box_le)
+        
 
         self.setLayout(v_box)
         self.setWindowTitle('TB')
@@ -29,14 +40,26 @@ class Window(QtWidgets.QWidget):
         self.button1.clicked.connect(self.button1_click)
         self.button2.clicked.connect(self.button2_click)
 
+        # Connect line edit signals and slots
+        self.button3.clicked.connect(self.lineEdit.clear)
+        self.lineEdit.returnPressed.connect(self.le_returnPressed)
+        self.lineEdit.cursorPositionChanged.connect(self.le_cursorChanged)
+
         # display window
         self.show()
 
     def button1_click(self):
         self.label1.setText(str(int(self.label1.text()) - 1))
-    
+    r
     def button2_click(self):
         self.label1.setText(str(int(self.label1.text()) + 1))
+
+    def le_returnPressed(self):
+        self.lineEdit.clear()
+    
+    def le_cursorChanged(self, old, new):
+        self.label1.setText(str(new))
+
 
 # Run the app
 app = QtWidgets.QApplication(sys.argv)

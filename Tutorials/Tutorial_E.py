f4892cbf6a93a6fe82340d49993f433c31cd562a
Muchen He <mansurhe@outlook.com>
2017-12-24 23:18:38 -0800

    Updated tutorial file with menu bar

--- Tutorials/Tutorial_E.py
+++ Tutorials/Tutorial_E.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from PyQt5.QtWidgets import *
 
 class MenuDemo(QMainWindow):
@@ -13,10 +14,12 @@ class MenuDemo(QMainWindow):
         menu_edit = menuBar.addMenu('Edit')
 
         # actions for menus
-        save_action = QAction('&Save', self)
-        save_action.setShortcut('Ctrl+S')
         new_action = QAction('&New', self)
         new_action.setShortcut('Ctrl+N')
+        open_action = QAction('&Open', self)
+        open_action.setShortcut('Ctrl+O')
+        save_action = QAction('&Save', self)
+        save_action.setShortcut('Ctrl+S')
         quit_action = QAction('&Quit', self)
         quit_action.setShortcut('Ctrl+Q')
         find_action = QAction('&Find', self)
@@ -26,25 +29,57 @@ class MenuDemo(QMainWindow):
 
         # Bind actions to menus
         menu_file.addAction(new_action)
+        menu_file.addAction(open_action)
         menu_file.addAction(save_action)
         menu_file.addAction(quit_action)
         menu_edit.addAction(find_action)
         menu_edit.addAction(replace_action)
 
         # Bind events from actions signals to slots
+        new_action.triggered.connect(self.new_trigger)
+        open_action.triggered.connect(self.open_trigger)
+        save_action.triggered.connect(self.save_trigger)
         quit_action.triggered.connect(self.quit_trigger)
-        menu_file.triggered.connect(self.selected)
 
+        find_action.triggered.connect(self.unbinded)
+        replace_action.triggered.connect(self.unbinded)
+    
         # Finalize window
         self.setWindowTitle('Menu Test')
         self.resize(600, 400)
         self.show()
 
+    # Slots for menu actions
+    def new_trigger(self):
+        """This should clear everything in the workspace"""
+        # TODO:
+
+    def open_trigger(self):
+        """Opens a dialog to select the file"""
+        fname = QFileDialog.getOpenFileName(self, 'Open file', os.getenv('HOME'), 'Text files (*.txt)')
+        if fname[0] != '':
+            with open(fname[0], 'r') as f:
+                text = f.read()
+                print(text)
+                # TODO: do something with the text
+
+    def save_trigger(self):
+        """Opens a dialog to save the file"""
+        fname = QFileDialog.getSaveFileName(self, 'Open file', os.getenv('HOME'), 'Text files (*.txt)')
+        if fname[0] != '':
+            with open(fname[0], 'w') as f:
+                text = "Lorem ipsum" #TODO:
+                f.write(text)
+                # TODO: do something with the text
+
     def quit_trigger(self):
+        """Exit the app"""
         qApp.quit()
 
-    def selected(self, q):
-        print(q.text() + 'selected')
+    def unbinded(self):
+        """Placeholder for unbinded signals"""
+        print("WIP")
+
 
 # Run the app
 app = QApplication(sys.argv)

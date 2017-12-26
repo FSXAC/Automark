b214fe53dad2672518627fcdf1356f3b65e267e9
Muchen He <mansurhe@outlook.com>
2017-12-26 15:14:21 -0800

    Added dockable widgets

--- Automark.py
+++ Automark.py
@@ -1,97 +1,88 @@
 import sys
 import os
-from PyQt5 import QtCore, QtGui
+# from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
-VERSION_NO = 'v0.0'
-
-class AutomarkWidget(QWidget):
-    def __init__(self):
-        super(AutomarkWidget, self).__init__()
-        
-        self.setupUi()
-    
-    def setupUi(self):
-        h_layout = QHBoxLayout()
-        self.setLayout(h_layout)
-        self.show()
-
-    def openFolder(self):
-        """Opens a dialog window to get file directory"""
-        fname = QFileDialog.getExistingDirectory(
-            self, 'Open folder', os.getenv('HOME')
-        )
-
-        #TODO: verify that the folder contains the right stuff
-        #TODO: then populate backend
-
-        return fname
+VERSION_NO = 'v0.0a'
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        
         # Using main automark widget as central widget
-        self.am = AutomarkWidget()
-        self.setCentralWidget(self.am)
+        # self.am = AutomarkWidget()
+        # self.setCentralWidget(self.am)
 
         self.setWindowTitle('Automark ' + VERSION_NO)
         self.setupUi()
 
-
     def setupUi(self):
-        # menuBar = self.menuBar()
-        # Make instance of menu and status bar
-        menubar = QMenuBar(self)
-
-        # Add root menus to menubar 
-        menuFile = menubar.addMenu('File')
-        menuEdit = menubar.addMenu('Edit')
-
-        # Setup menu actions
+        # Menu and status bar
+        self.menubar = QMenuBar(self)
+        self.statusbar = QStatusBar(self)
+        menuFile = self.menubar.addMenu('File')
+        menuView = self.menubar.addMenu('View')
         self.setupMenuFile(menuFile)
-        self.setupMenuEdit(menuEdit)
+        self.setupMenuView(menuView)
+        self.setMenuBar(self.menubar)
+        self.setStatusBar(self.statusbar)
 
-        # Add menu bar to main window
-        self.setMenuBar(menubar)
+        # Toolbar
+        self.toolbar = self.addToolBar('Files')
 
-        self.resize(600, 400)
+        # Docks
+        # self.dockFolders = QDockWidget(self)
+        self.setupDocks()
+
+        # Widgets
+        self.textedit = QTextEdit()
+        self.setCentralWidget(self.textedit)
+
+
+        # Window
+        self.resize(800, 600)
         self.show()
 
     def setupMenuFile(self, menu):
-        actNew = QAction('&New', self)
-        actNew.setShortcut('Ctrl+N')
-        actOpen = QAction('&Open folder', self)
-        actOpen.setShortcut('Ctrl+O')
-        actSave = QAction('&Save', self)
-        actSave.setShortcut('Ctrl+S')
-        actQuit = QAction('&Quit', self)
-        actQuit.setShortcut('Ctrl+Q')
+        actionOpenFolder = QAction('&Open Folder', self)
+        actionQuit = QAction('&Quit', self)
 
         # Add menu actions to menu
-        menu.addAction(actNew)
-        menu.addAction(actOpen)
-        menu.addAction(actSave)
-        menu.addAction(actQuit)
+        menu.addAction(actionOpenFolder)
+        menu.addSeparator()
+        menu.addAction(actionQuit)
 
         # Bind signals from actions to slots
         menu.triggered.connect(self.menuFileHandler)
     
-    def setupMenuEdit(self, menu):
-        """Nothing here yet"""
+    def setupMenuView(self, menu):
+        menu.triggered.connect(self.menuViewHandler)
+
+    def setupDocks(self):
+        self.setupDockFolders()
+
+    def setupDockFolders(self):
+        self.dockFolders = QDockWidget(self)
+        self.dockFoldersContent = QWidget()
+        vLayout = QVBoxLayout(self.dockFoldersContent)
+        testLabel = QLabel(self.dockFoldersContent)
+        vLayout.addWidget(testLabel)
+
+        self.dockFolders.setWidget(self.dockFoldersContent)
+        self.dockFolders.setWindowTitle('Folders')
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.dockFolders)
 
     def menuFileHandler(self, sender):
         """Handles events of menu items being clicked on"""
         signal = sender.text()
-        if signal == '&New':
-            print('New handler here')
-        elif signal == '&Open folder':
-            directory = self.am.openFolder()
-            print(directory)
-        elif signal == '&Save':
-            print('save')
+        if signal == '&Open Folder':
+            print('Open folder handler here')
         elif signal == '&Quit':
             qApp.quit()
+    
+    def menuViewHandler(self, sender):
+        print(sender.text())
 
 # Run the app
 app = QApplication(sys.argv)

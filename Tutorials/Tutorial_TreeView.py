71164576b2b15d146c34d68a996685220babdc51
Muchen He <mansurhe@outlook.com>
2017-12-30 21:25:28 -0800

    Select items in a treeview

--- Tutorials/Tutorial_TreeView.py
+++ Tutorials/Tutorial_TreeView.py
@@ -21,24 +21,20 @@ class App(QWidget):
 
         # Set data group
         self.treeview = QTreeView()
-        # self.treeview.setRootIsDecorated(False)
+        self.treeview.setRootIsDecorated(False)
         # self.treeview.setAlternatingRowColors(True)
 
         # Create and set model
         model = self.createMailModel()
         self.treeview.setModel(model)
+        self.treeview.clicked.connect(
+            lambda x: self.itemSelected(model, x)
+        )
 
         # Populate the model with some arbitrary data
         self.addMail(model, '1@a.com', 'SUBJECT A', '2017-12-31')
         self.addMail(model, '2@a.com', 'SUBJECT B', '2017-12-31')
         self.addMail(model, '3@a.com', 'SUBJECT C', '2017-12-31')
-
-        model.item(0).setCheckable(True)
-        model.item(1).setEditable(False)
-        model.item(2).setEnabled(False)
-
-        model.itemChanged.connect(self.onItemChanged)
-        
 
         # Put things in window
         mainLayout = QVBoxLayout()
@@ -58,9 +54,25 @@ class App(QWidget):
         model.setData(model.index(0, self.FROM), sender)
         model.setData(model.index(0, self.SUBJECT), subject)
         model.setData(model.index(0, self.DATE), date)
-        
-    def onItemChanged(self, item):
-        print(item)
+
+        model.item(0).setCheckable(True)
+        model.item(0, self.FROM).setEditable(False)
+        model.item(0, self.SUBJECT).setEditable(False)
+        model.item(0, self.DATE).setEditable(False)
+        # model.item(0).setChecked(False)
+
+    def itemSelected(self, model, modelIndex):
+        # Only care about the first column
+        item = model.item(modelIndex.row())
+
+        if item.checkState() == Qt.Checked:
+            item.setCheckState(Qt.Unchecked)
+        else:
+            item.setCheckState(Qt.Checked)
+
+        item = model.item(modelIndex.row(), self.SUBJECT)
+        item.setText(item.text() + ' (DONE)')
+
 
 # Run app
 if __name__ == '__main__':

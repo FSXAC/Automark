cf7a1b66f7e44d693dc39549b0f415089b92f445
Muchen He <mansurhe@outlook.com>
2018-01-15 12:18:59 -0800

    Updated action manager

--- ActionManager.py
+++ ActionManager.py
@@ -9,17 +9,17 @@ from PyQt5.QtGui import QIcon, QFont, QStandardItem, QStandardItemModel
 class ActionManager():
     def __init__(self, parent):
         """Constructor"""
-        self.openFolder = self.create_action('&Open Folder')
         self.parent = parent
+        self.make_actions()
 
     def create_action(
-        self, 
-        name, 
-        checkable=False, 
-        checked=False, 
-        enabled=True,
-        shortcut='',
-        icon=''
+            self,
+            name,
+            checkable=False,
+            checked=False,
+            enabled=True,
+            shortcut='',
+            icon=''
         ):
         """Returns a QAction instance with ActionManager as parent"""
         new_action = QAction(name, self.parent)
@@ -32,3 +32,20 @@ class ActionManager():
             action_icon = QIcon(icon)
             new_action.seticon(action_icon)
         return new_action
+
+    def make_actions(self):
+        """ Instantiates all the necessary actions required to run the app"""
+        
+        # File actions
+        self.act_open_folder = self.create_action('&Open Folders', icon='hi.png', shortcut='Ctrl+O')
+        self.act_quit = self.create_action('&Quit', shortcut='Ctrl+Q')
+
+        # View actions
+        self.act_view_summary = self.create_action('Folders', checkable=True, checked=True)
+        self.act_view_submission = self.create_action('Submission', checkable=True, checked=True)
+        self.act_view_note = self.create_action('Note', checkable=True, checked=True)
+        self.act_view_verdict = self.create_action('Verdict', checkable=True, checked=True)
+
+        # Compilation actions
+        self.act_compile_run = self.create_action('Compile and Run')
+        
